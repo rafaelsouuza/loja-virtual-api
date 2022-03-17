6822128684df7bc98ae366d638b657cb55d7f582
Rafael Souza <snrafa.souza@gmail.com>
2022-03-17 19:13:52 -0300

    Projeto criado

---
+++ src/main/java/io/github/rafaelsouuza/lojavirtual/api/LojaVirtualApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rafaelsouuza.lojavirtual.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaVirtualApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rafaelsouuza/lojavirtual/api/LojaVirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafaelsouuza.lojavirtual.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
