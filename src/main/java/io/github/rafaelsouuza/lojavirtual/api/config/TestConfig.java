22f6868b6e65d13df56144cc40ebe2327fed38ed
Rafael Souza <snrafa.souza@gmail.com>
2022-03-18 01:28:50 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/io/github/rafaelsouuza/lojavirtual/api/config/TestConfig.java
+++ src/main/java/io/github/rafaelsouuza/lojavirtual/api/config/TestConfig.java
@@ -1,13 +1,8 @@
 package io.github.rafaelsouuza.lojavirtual.api.config;
 
-import io.github.rafaelsouuza.lojavirtual.api.entities.Categoria;
-import io.github.rafaelsouuza.lojavirtual.api.entities.Cidade;
-import io.github.rafaelsouuza.lojavirtual.api.entities.Estado;
-import io.github.rafaelsouuza.lojavirtual.api.entities.Produto;
-import io.github.rafaelsouuza.lojavirtual.api.repositories.CategoriaRepository;
-import io.github.rafaelsouuza.lojavirtual.api.repositories.CidadeRepository;
-import io.github.rafaelsouuza.lojavirtual.api.repositories.EstadoRepository;
-import io.github.rafaelsouuza.lojavirtual.api.repositories.ProdutoRepository;
+import io.github.rafaelsouuza.lojavirtual.api.entities.*;
+import io.github.rafaelsouuza.lojavirtual.api.entities.enums.TipoCliente;
+import io.github.rafaelsouuza.lojavirtual.api.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +25,12 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -54,15 +55,26 @@ public class TestConfig implements CommandLineRunner {
         Estado est1 = new Estado(null, "Minas Gerais");
         Estado est2 = new Estado(null, "São Paulo");
 
-        Cidade cid1 = new Cidade(null, "Uberlândia", est1);
-        Cidade cid2 = new Cidade(null, "São Paulo", est2);
-        Cidade cid3 = new Cidade(null, "Campinas", est2);
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
 
-        est1.getCidades().addAll(Arrays.asList(cid1));
-        est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
 
         estadoRepository.saveAll(Arrays.asList(est1, est2));
-        cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "3865385534", TipoCliente.PESSOAFISICA);
+
+        cli1.getTelefones().addAll(Arrays.asList("34523535", "35353535"));
+
+        Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "348765835", cli1, c1);
+        Endereco e2 =new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "86935938", cli1, c2);
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
     }
 }
