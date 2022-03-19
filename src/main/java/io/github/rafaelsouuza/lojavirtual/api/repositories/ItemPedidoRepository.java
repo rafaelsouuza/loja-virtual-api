4290478bca6ae4dcf9c9cbc94e544475ade825b1
Rafael Souza <snrafa.souza@gmail.com>
2022-03-18 21:36:28 -0300

    ItemPedido e ItemPedidoPk

---
+++ src/main/java/io/github/rafaelsouuza/lojavirtual/api/repositories/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package io.github.rafaelsouuza.lojavirtual.api.repositories;
+
+import io.github.rafaelsouuza.lojavirtual.api.entities.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
