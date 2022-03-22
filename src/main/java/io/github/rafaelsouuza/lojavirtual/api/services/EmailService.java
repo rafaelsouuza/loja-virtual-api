6956dc20ee767001f922519b6d59779f3d1aa0dc
Rafael Souza <snrafa.souza@gmail.com>
2022-03-22 19:30:38 -0300

    Email HTML

--- src/main/java/io/github/rafaelsouuza/lojavirtual/api/services/EmailService.java
+++ src/main/java/io/github/rafaelsouuza/lojavirtual/api/services/EmailService.java
@@ -3,9 +3,15 @@ package io.github.rafaelsouuza.lojavirtual.api.services;
 import io.github.rafaelsouuza.lojavirtual.api.entities.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
