246eb17c9375dbb71e49ef5dfce2cd20b6816247
Rafael Souza <snrafa.souza@gmail.com>
2022-03-23 16:01:41 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/io/github/rafaelsouuza/lojavirtual/api/config/SecurityConfig.java
+++ src/main/java/io/github/rafaelsouuza/lojavirtual/api/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package io.github.rafaelsouuza.lojavirtual.api.config;
 
+import io.github.rafaelsouuza.lojavirtual.api.security.JWTAuthenticationFilter;
+import io.github.rafaelsouuza.lojavirtual.api.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -21,7 +25,13 @@ import java.util.Arrays;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     private Environment env;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     private static String[] PUBLIC_MATCHERS = {
             "/h2-console/**"
@@ -45,7 +55,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
